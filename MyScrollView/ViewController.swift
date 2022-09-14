1e013e26d67d94dd96a6259ed02142289532c817
broMac <yhs4705@naver.com>
2022-09-14 20:32:39 +0900

    키보드 테스트

--- MyScrollView/ViewController.swift
+++ MyScrollView/ViewController.swift
@@ -10,72 +10,134 @@ import NotificationCenter
 
 class ViewController: UIViewController, UIScrollViewDelegate {
     
-    var sharedValue = 1
 
+    @IBOutlet weak var textView: UITextView!
+    
+    var keyboardHeight: CGFloat?
+
+    @IBOutlet weak var scrollview: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        textView.isScrollEnabled = false
+        textView.delegate = self
         
-    
-        var a : Test = B.init()
-        let b = B()
+        print("작동중?")
         
-        let bes = UIView()
-        bes.clipsToBounds = true
-        bes.layer.masksToBounds = true
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShowNotification(keyboard:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHideNotification(keyboard:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+    }
 
+    @objc func keyboardWillShowNotification(keyboard: NSNotification){
+        let userInfo = keyboard.userInfo
+        let keyboardFrame = userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
+        keyboardHeight = keyboardFrame.height
+
+       calculateKeyboardHeight()
     }
     
-    func changeTest() {
-        DispatchQueue.global(qos: .background).async {
-            print("background", Thread.current)
-            var newValue = self.sharedValue
-            print("background Before", newValue)
-            newValue += 1
-            self.sharedValue = newValue
-            print("background after", self.sharedValue)
+    @objc func keyboardWillHideNotification(keyboard: NSNotification){
+        self.view.frame.origin.y = 0
+    }
+    
+    
+    @IBAction func tapGestureClicked(_ sender: UITapGestureRecognizer) {
+        self.textView.resignFirstResponder()
+    }
+
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        view.endEditing(true)
+    }
+    
+    
+    func calculateKeyboardHeight() {
+        print("동작")
+        let caret = textView.caretRect(for: textView.selectedTextRange!.start)
+         print("\(caret.origin.y)")
+        let keyboardTopBorder = textView.bounds.size.height - (keyboardHeight ?? 0)
+        
+        
+        //caret.origin.y < keyboardTopBorder
+        
+        //남은 공간
+        let size = textView.bounds.size.height - caret.origin.y
+        print(size)
+        
+        guard let keyboardHeight = keyboardHeight else { return }
+        if size < keyboardHeight {
+            print("작다")
+            textView.layoutIfNeeded()
+
+            self.view.frame.origin.y = 0
+            self.view.frame.origin.y -= keyboardHeight
+            //textView.scrollRectToVisible(caret, animated: true)
+            scrollview.scrollToBottom()
+            
+        } else {
+            print("크 다")
+            //textView.scrollRectToVisible(caret, animated: true)
+            //self.view.frame.origin.y = 0
+            //self.view.frame.origin.y -= keyboardHeight ?? 0
         }
-        
-        DispatchQueue.global(qos: .userInitiated).async {
-            print("userInitiated", Thread.current)
-            var newValue = self.sharedValue
-            print("userInitiated Before", newValue)
-            newValue += 1
-            self.sharedValue = newValue
-            print("userInitiated after", self.sharedValue)
+    }
+    
+}
+
+extension ViewController: UITextViewDelegate {
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        self.view.frame.origin.y = 0
+    }
+    
+//    func textViewShouldBeginEditing(_ textView: UITextView) -> Bool {
+//        calculateKeyboardHeight()
+//        return true
+//    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        calculateKeyboardHeight()
+    }
+
+}
+
+public enum ScrollDirection {
+    case top
+    case center
+    case bottom
+}
+
+
+public extension UIScrollView {
+
+    func scroll(to direction: ScrollDirection) {
+
+        DispatchQueue.main.async {
+            switch direction {
+            case .top:
+                self.scrollToTop()
+            case .center:
+                self.scrollToCenter()
+            case .bottom:
+                self.scrollToBottom()
+            }
         }
     }
 
+    func scrollToTop() {
+        setContentOffset(.zero, animated: true)
+    }
 
+    func scrollToCenter() {
+        let centerOffset = CGPoint(x: 0, y: (contentSize.height - bounds.size.height) / 2)
+        setContentOffset(centerOffset, animated: true)
+    }
 
-}
-
-protocol Test {
-    static var test: String { get set }
-    func hi()
-}
-
-extension Test {
-    func hi() {
-        print("test")
+    func scrollToBottom() {
+        let bottomOffset = CGPoint(x: 0, y: contentSize.height - bounds.size.height + contentInset.bottom)
+        if(bottomOffset.y > 0) {
+            setContentOffset(bottomOffset, animated: true)
+        }
     }
 }
-
-class A: Test {
-    static var test: String = "ss"
-    
-}
-
-class B: Test {
-    static var test: String = "ss"
-    
-    func hi() {
-        print("bb")
-    }
-}
-
-class c {
-    func extensionOverrideTest() {
-        
-    }
-}
-
